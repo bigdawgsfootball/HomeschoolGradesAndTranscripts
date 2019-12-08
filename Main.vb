e7f6e9e808f85523967ce66062b76de6c89b8e01
Kurt Hockaday <bigdawgsfootball@live.com>
2019-12-07 19:59:09 -0500

    put files in wrong place
    removing files in root dir

--- Main.vb
+++
@@ -1,6 +0,0 @@
-﻿
-Module Main
-    Friend GB As New Gradebook
-    Friend GBFile As String = Environment.GetEnvironmentVariable("GBLOCATION", EnvironmentVariableTarget.User)
-    Friend AD As AppData
-End Module
